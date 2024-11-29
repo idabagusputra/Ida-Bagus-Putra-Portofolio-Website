ec9cac31f6a342144f1007fcd6ad79f63e056626
Ida Bagus Putu Putra Manuaba <ibp.putra.m@gmail.com>
2024-11-29 22:18:51 +0700

    Porto Update About

--- src/app/(root)/(app)/about/page.tsx
+++ src/app/(root)/(app)/about/page.tsx
@@ -7,36 +7,7 @@ import { RESUME_URL } from '@/constans/common'
 import PageTitle from '../components/PageTitle'
 import React, { useEffect, useRef, ReactNode } from 'react'
 import { motion, useInView, useAnimation } from 'framer-motion'
-
-interface AnimatedSectionProps {
-  children: ReactNode
-}
-
-export const AnimatedSection: React.FC<AnimatedSectionProps> = ({ children }) => {
-  const ref = useRef(null)
-  const isInView = useInView(ref, { once: true })
-  const controls = useAnimation()
-
-  useEffect(() => {
-    if (isInView) {
-      controls.start('visible')
-    }
-  }, [isInView, controls])
-
-  return (
-    <motion.div
-      ref={ref}
-      animate={controls}
-      initial="hidden"
-      variants={{
-        hidden: { opacity: 0, y: 50 },
-        visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
-      }}
-    >
-      {children}
-    </motion.div>
-  )
-}
+import { AnimatedSection } from './AnimatedSection'
 
 const ExperienceTimeline = () => {
   const experiences = [
